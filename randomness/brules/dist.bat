2caaff76f1127ae5fd62f57accc7033a9fe7510d
Mike Travers <mt@alum.mit.edu>
2012-03-04 09:30:08 -0800

    cleanup

--- randomness/brules/dist.bat
+++
@@ -1,7 +0,0 @@
-echo make BRules distribution
-
-cd d:\java\misc\com\ibm\jikes\skij\brules
-clean
-cd d:\java\misc
-rm d:\mt\release\brules.zip
-jar cvfM d:\mt\release\brules.zip com\ibm\jikes\skij\brules\*
